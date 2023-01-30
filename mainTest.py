65e39fafac3e0cb0f7fd87c545d91b1379e177d8
mannanej <mannanej@rose-hulman.edu>
2023-01-30 21:12:33 +0000

    Added time calculations

--- mainTest.py
+++ mainTest.py
@@ -12,12 +12,17 @@ def main():
     fileNames = os.listdir(pathName)
     if fileNames[0].endswith(".csv"):
         pathName += fileNames[0]
+    else:
+        print("ERROR! No .csv file found")
+        return
     filename = open(pathName, "r")
     file = csv.DictReader(filename)
     # This section will get the column of sugar values
     sugars = getSugarsFromFile(file)
     # This section will get the date
-    date = getDate(fileNames[0])
+    currentDate = getDate(fileNames[0])
+    # This section will get the time
+    currentTime = getTime(fileNames[0])
     # This section will get the A1C value from these sugars
     a1c = getA1C(sugars)
     # This section will get the Highest sugar
@@ -27,10 +32,11 @@ def main():
 
     endTime = time.perf_counter()
     runTime = endTime - startTime
-# General print statements for debugging
+    # General print statements for debugging
     # print(sugars)
     # print(len(sugars))
-    print("Date: ", date.strftime('%m-%d-%Y'))
+    print("Date: ", currentDate.strftime('%m-%d-%Y'))
+    print("Time: ", currentTime)
     print("A1C: ", a1c)
     print("Highest: ", highest)
     print("Lowest: ", lowest)
@@ -63,13 +69,59 @@ def getAverage(sugars):
 ####################################################################################################################################
 def getDate(fileName):
     if fileName.endswith(".csv"):
-        date = datetime(year = int(fileName[29:33]), month = int(fileName[34:36]), day = int(fileName[37:39]))
-    return date
+        currentDate = datetime(year = int(fileName[29:33]), month = int(fileName[34:36]), day = int(fileName[37:39]))
+    return currentDate
+####################################################################################################################################
+def getTime(fileName):
+    if fileName.endswith(".csv"):
+        hour = fileName[40:42]
+        minute = fileName[42:44]
+        second = fileName[44:46]
+        if (hour == "04"):
+            hour = "23"
+        elif (hour == "03"):
+            hour = "22"
+        elif (hour == "02"):
+            hour = "21"
+        elif (hour == "01"):
+            hour = "20"
+        elif (hour == "00"):
+            hour = "19"
+        else:
+            hour = str(int(hour) - 5)
+        # easternTimeMilitary = hour + "::" + minute + "::" + second
+        if (hour == "13"):
+            hour = "1"
+        elif (hour == "14"):
+            hour = "2"
+        elif (hour == "15"):
+            hour = "3"
+        elif (hour == "16"):
+            hour = "4"
+        elif (hour == "17"):
+            hour = "5"
+        elif (hour == "18"):
+            hour = "6"
+        elif (hour == "19"):
+            hour = "7"
+        elif (hour == "20"):
+            hour = "8"
+        elif (hour == "21"):
+            hour = "9"
+        elif (hour == "22"):
+            hour = "10"
+        elif (hour == "23"):
+            hour = "11"
+        elif (hour == "00"):
+            hour = "12"
+        easternTimeStandard = hour + "::" + minute + "::" + second
+        currentTime = datetime.strptime(easternTimeStandard, '%H::%M::%S').time()
+    return currentTime
 ####################################################################################################################################
 def getHighestSugar(sugars):
     sugars.sort()
     return sugars[-1]
-
+####################################################################################################################################
 def getLowestSugar(sugars):
     sugars.sort()
     return sugars[0]
