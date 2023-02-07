99a7a43d447ee044e6c491a473dc953d4f59a0a0
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2023-02-07 20:50:08 +0000

    added install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# File Name: install.sh
+# Author: David Mattingly, Eddie Mayfield
+# Date Created: 07FEB2023
+# Date last edited: 07FEB2023
+# Topic: ECE434 Embedded Linux, Dr. Yoder
+# Project: Diabetic Monitor
+# Description: 
+
+apt install python3-numpy
+apt install python3-csv
+apt install python3-datetime
